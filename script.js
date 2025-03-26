8708c18b9d052b47f04ccda769035dc92384eb03
FlukeModify <flukessd1@gmail.com>
2025-03-27 00:41:47 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// You can add interactivity here if needed
+// Example: Add smooth scrolling effect when clicking links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
